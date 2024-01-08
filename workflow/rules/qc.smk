dfc8edc76d2852fe8a1bf5576c22ce00603e8961
Anthony Tan <atan5133@gmail.com>
2024-01-08 12:39:50 -0800

    updated code to get qc plots for v1 of encode

--- workflow/rules/qc.smk
+++ workflow/rules/qc.smk
@@ -1,6 +1,13 @@
+import re
+import glob
+re_pattern = re.compile("/oak/stanford/groups/engreitz/Users/agschwin/distal_regulation_paper/ENCODE_portal_submission/encode_e2g/results/dnase_only/thresholded_predictions/encode_e2g_predictions_(.*)_DNaseOnly_thresholded_predictions.tsv.gz")
+files = glob.glob('/oak/stanford/groups/engreitz/Users/agschwin/distal_regulation_paper/ENCODE_portal_submission/encode_e2g/results/dnase_only/thresholded_predictions/encode_e2g_predictions*thresholded_predictions.tsv.gz')
+
+BIOSAMPLES = [re_pattern.match(file).group(1) for file in files]
+
 rule get_stats:
 	input:
-		thresholded = os.path.join(RESULTS_DIR, "{biosample}", "Predictions", "encode_e2g_predictions_threshold{threshold}.tsv.gz")
+		thresholded = "/oak/stanford/groups/engreitz/Users/agschwin/distal_regulation_paper/ENCODE_portal_submission/encode_e2g/results/dnase_only/thresholded_predictions/encode_e2g_predictions_{biosample}_DNaseOnly_thresholded_predictions.tsv.gz"
 	params:
 		scripts_dir = SCRIPTS_DIR
 	conda:
@@ -8,7 +15,7 @@ rule get_stats:
 	resources:
 		mem_mb=determine_mem_mb
 	output:
-		stats = os.path.join(RESULTS_DIR, "{biosample}", "Metrics", "encode_e2g_predictions_threshold{threshold}_stats.tsv")
+		stats = os.path.join(RESULTS_DIR, "{biosample}", "Metrics", "encode_e2g_predictions_threshold_stats.tsv")
 	shell:
 		"""
 		python {params.scripts_dir}/get_stats.py --predictions {input.thresholded} --output_file {output.stats}
@@ -16,7 +23,7 @@ rule get_stats:
 
 rule generate_plots:
 	input:
-		stat_files = [os.path.join(RESULTS_DIR, f"{biosample}", "Metrics", f"encode_e2g_predictions_threshold{get_threshold(biosample)}_stats.tsv") for biosample in BIOSAMPLES]
+		stat_files = [os.path.join(RESULTS_DIR, f"{biosample}", "Metrics", f"encode_e2g_predictions_threshold_stats.tsv") for biosample in BIOSAMPLES]
 	params:
 		scripts_dir = SCRIPTS_DIR
 	conda:
--- workflow/scripts/get_stats.py
+++ workflow/scripts/get_stats.py
@@ -1,52 +1,62 @@
-import numpy as np
-
 import click
+import numpy as np
 import pandas as pd
+
 
 def get_num_enh(df):
     return len(df[["chr", "start", "end"]].drop_duplicates())
 
+
 def get_num_genes(df):
     return len(df["TargetGene"].drop_duplicates())
 
+
 def get_num_enh_gene_links(df):
     return len(df)
+
 
 def get_num_genes_with_1_enh_min(df):
     genes = df.groupby(["TargetGene"]).size()
     return len(genes[genes > 0])
 
+
 def get_mean_num_genes_per_enh(df):
     enh = df[["chr", "start", "end"]].groupby(["chr", "start", "end"]).size()
     return enh.mean()
 
+
 def get_mean_num_enh_per_gene(df):
     genes = df.groupby(["TargetGene"]).size()
     return genes.mean()
+
 
 def get_mean_num_enh_per_gene_no_prom(df):
     df = df[df["class"] != "promoter"]
     genes = df.groupby(["TargetGene"]).size()
     return genes.mean()
 
+
 def get_mean_log_dist_to_tss(df):
-    log_dist = df["distanceToTSS"].apply(np.log10)
+    log_dist = df["distanceToTSS.Feature"].apply(np.log10)
     log_dist = log_dist.replace(-np.inf, 0)
     return log_dist.mean()
 
-def get_mean_enh_region_size(df):  
+
+def get_mean_enh_region_size(df):
     enh = df.groupby(["chr", "start", "end"])
     sizes = []
     for (_, start, end), _ in enh:
-        sizes.append(end-start)
+        sizes.append(end - start)
     sizes = pd.Series(sizes)
     return sizes.mean()
+
 
 @click.command()
 @click.option("--predictions", type=str, required=True)
 @click.option("--output_file", type=str, default="stats.tsv")
 def main(predictions, output_file):
     df = pd.read_csv(predictions, sep="\t")
+    df.rename(columns={"#chr": "chr"}, inplace=True)
     stats = [
         ("num_enh", get_num_enh(df)),
         ("num_genes", get_num_genes(df)),
@@ -62,6 +72,7 @@ def main(predictions, output_file):
     values = [stat[1] for stat in stats]
     df = pd.DataFrame({"Metric": metric, "Value": values})
     df.to_csv(output_file, sep="\t", index=False)
+
 
 if __name__ == "__main__":
     main()
