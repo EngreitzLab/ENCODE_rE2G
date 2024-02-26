0ba72a57bdecabe026424747a905093830b840f9
Maya Sheth <shethm@sh03-ln06.stanford.edu>
2024-02-26 08:36:59 -0800

    update scripts

--- workflow/scripts/plot_model_comparison.R
+++ workflow/scripts/plot_model_comparison.R
@@ -11,6 +11,8 @@ output_file_auprc = (snakemake@output$comp_plot_auprc)
 output_file_prec = (snakemake@output$comp_plot_prec)
 
 df = fread(input_file, sep="\t")
+use_colors = dplyr::n_distinct(df$dataset) <= 10 # only color by dataset if 10 or fewer datasets
+df$dataset = ifelse(use_colors, df$dataset, 'filler')
 
 # plot auprc
 auprc = ggplot(df, aes(x = reorder(model, -AUPRC), y = AUPRC, fill = dataset)) +
@@ -21,8 +23,6 @@ auprc = ggplot(df, aes(x = reorder(model, -AUPRC), y = AUPRC, fill = dataset)) +
   guides(fill = guide_legend(title = "Dataset")) +
   coord_flip() +
   theme_classic() + theme(axis.text = element_text(size = 7), axis.title = element_text(size = 8), legend.position='bottom')
-auprc = set_palette(auprc, 'npg')
-ggsave(filename=output_file_auprc, plot=auprc, width=6, height=4)
 
 # plot precision
 prec = ggplot(df, aes(x = reorder(model, -precision), y = precision, fill = dataset)) +
@@ -33,8 +33,14 @@ prec = ggplot(df, aes(x = reorder(model, -precision), y = precision, fill = data
   guides(fill = guide_legend(title = "Dataset")) +
   coord_flip() +
   theme_classic() + theme(axis.text = element_text(size = 7), axis.title = element_text(size = 8), legend.position='bottom')
+
+if (!use_colors) {
+    auprc = auprc + theme(legend.position='None')
+    prec = prec + theme(legend.position='None')
+}
+auprc = set_palette(auprc, 'npg')
 prec = set_palette(prec, 'npg')
+
+ggsave(filename=output_file_auprc, plot=auprc, width=6, height=4)
 ggsave(filename=output_file_prec, plot=prec, width=6, height=4)
-
-
 
