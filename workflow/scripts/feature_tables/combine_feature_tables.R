497e7344e2f9f88a523307a8d1589c0a04f10028
Maya Sheth <shethm@sh03-ln01.stanford.edu>
2024-06-01 10:42:52 -0700

    scE2G compatability

--- workflow/scripts/feature_tables/combine_feature_tables.R
+++ workflow/scripts/feature_tables/combine_feature_tables.R
@@ -12,5 +12,12 @@ for (i in 1:nrow(models_this)){
     ft = fread(models_this$feature_table[i])
     if (i==1){df = ft} else {df = rbind(df, ft)}
 }
+
+# for sc-E2G pipeline
+if ("ARC.E2G.Score" %in% df$feature){
+	Kendall_row = data.frame("Kendall", "Kendall", NA, "max", "overlap", "Kendall correlation"); names(Kendall_row) = colnames(df)
+	df = rbind(df, Kendall_row)
+}
+
 df = dplyr::distinct(df)
 fwrite(df, file = snakemake@output$dataset_features, sep = "\t")
