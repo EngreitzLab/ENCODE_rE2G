49e91e367134dcd7d7dcb6af0d4563577a7ab7b2
Maya Sheth <shethm@sh03-ln01.stanford.edu>
2024-06-30 11:03:37 -0700

    make bedpe of predictions, filter based on score col

--- workflow/scripts/model_application/threshold_e2g_predictions.py
+++ workflow/scripts/model_application/threshold_e2g_predictions.py
@@ -2,9 +2,9 @@ import click
 import pandas as pd
 
 
-def threshold_predictions(all_putative, threshold, include_self_promoter):
+def threshold_predictions(all_putative, threshold, score_column, include_self_promoter):
     # filter by threshold and promoter category
-    filtered_predictions = all_putative[all_putative["ENCODE-rE2G.Score"] >= threshold]
+    filtered_predictions = all_putative[all_putative[score_column] >= threshold]
 
     # process promoters & self-promoters
     if include_self_promoter:
@@ -19,12 +19,13 @@ def threshold_predictions(all_putative, threshold, include_self_promoter):
 @click.command()
 @click.option("--all_predictions_file", required=True)
 @click.option("--threshold", type=float, required=True)
+@click.option("--score_column", type=float, required=True)
 @click.option("--include_self_promoter", type=bool, default=True)
 @click.option("--output_file", required=True)
-def main(all_predictions_file, threshold, include_self_promoter, output_file):
+def main(all_predictions_file, threshold, score_column, include_self_promoter, output_file):
     all_predictions = pd.read_csv(all_predictions_file, sep="\t")
     filtered_predictions = threshold_predictions(
-        all_predictions, threshold, include_self_promoter
+        all_predictions, threshold, score_column, include_self_promoter
     )
     filtered_predictions.to_csv(output_file, compression="gzip", sep="\t", index=False)
 
