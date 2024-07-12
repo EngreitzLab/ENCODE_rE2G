4e469938db13dfad6c3eb68d95ed42de6223f4c5
Maya Sheth <shethm@sh03-ln02.stanford.edu>
2024-07-12 11:30:34 -0700

    autoformat

--- workflow/scripts/feature_tables/gen_num_candidate_enh_gene.py
+++ workflow/scripts/feature_tables/gen_num_candidate_enh_gene.py
@@ -2,52 +2,55 @@ import os
 import click
 import pandas as pd
 
-def _populate_enhancer_count_from_tss(df, enhancers, is_upstream):
-	enh_indexes = enhancers.index
-	if is_upstream:
-		# start counting from the enhancer closest to TSS
-		enh_indexes = reversed(enh_indexes)
 
-	count_from_tss = 0
-	for enh_idx in enh_indexes:
-		count_from_tss += 1
-		df.loc[enh_idx, "NumCandidateEnhGene"] = count_from_tss
+def _populate_enhancer_count_from_tss(df, enhancers, is_upstream):
+    enh_indexes = enhancers.index
+    if is_upstream:
+        # start counting from the enhancer closest to TSS
+        enh_indexes = reversed(enh_indexes)
+
+    count_from_tss = 0
+    for enh_idx in enh_indexes:
+        count_from_tss += 1
+        df.loc[enh_idx, "NumCandidateEnhGene"] = count_from_tss
+
 
 def determine_num_candidate_enh_gene(pred_df, out_file):
-	# Need df to be sorted by midpoint for each chromosome
-	pred_df["midpoint"] = ((pred_df["start"] + pred_df["end"]) / 2).astype("int")
-	df = pred_df.sort_values(by=["chr", "midpoint"], ascending=True).reset_index(
-		drop=True
-	)
+    # Need df to be sorted by midpoint for each chromosome
+    pred_df["midpoint"] = ((pred_df["start"] + pred_df["end"]) / 2).astype("int")
+    df = pred_df.sort_values(by=["chr", "midpoint"], ascending=True).reset_index(
+        drop=True
+    )
 
-	gene_groups = df.groupby(["TargetGene", "TargetGeneTSS"])
-	for (gene, tss), indexes in gene_groups.groups.items():
-		enhancers = df.loc[indexes]
-		upstream_enh = enhancers[enhancers["midpoint"] < tss]
-		downstream_enh = enhancers[enhancers["midpoint"] > tss]
-		_populate_enhancer_count_from_tss(df, upstream_enh, is_upstream=True)
-		_populate_enhancer_count_from_tss(df, downstream_enh, is_upstream=False)
+    gene_groups = df.groupby(["TargetGene", "TargetGeneTSS"])
+    for (gene, tss), indexes in gene_groups.groups.items():
+        enhancers = df.loc[indexes]
+        upstream_enh = enhancers[enhancers["midpoint"] < tss]
+        downstream_enh = enhancers[enhancers["midpoint"] > tss]
+        _populate_enhancer_count_from_tss(df, upstream_enh, is_upstream=True)
+        _populate_enhancer_count_from_tss(df, downstream_enh, is_upstream=False)
 
-	df = df.fillna(value=0)
-	df["NumCandidateEnhGene"] = df["NumCandidateEnhGene"].astype("int")
-	df[["name", "TargetGene", "NumCandidateEnhGene"]].to_csv(
-		out_file,
-		sep="\t",
-		index=False,
-	)
-	print("Saved num candidate enhancers")
+    df = df.fillna(value=0)
+    df["NumCandidateEnhGene"] = df["NumCandidateEnhGene"].astype("int")
+    df[["name", "TargetGene", "NumCandidateEnhGene"]].to_csv(
+        out_file,
+        sep="\t",
+        index=False,
+    )
+    print("Saved num candidate enhancers")
 
 
 @click.command()
 @click.option("--abc_predictions")
 @click.option("--out_file")
 def main(abc_predictions, out_file):
-	pred_df = pd.read_csv(abc_predictions, sep="\t")
-	pred_df = pred_df[pred_df["class"] != "promoter"] # filter out promoters
-	if len(pred_df) == 0:
-		raise Exception("Did not find any enhancers in the Predictions file")
+    pred_df = pd.read_csv(abc_predictions, sep="\t")
+    pred_df = pred_df[pred_df["class"] != "promoter"]  # filter out promoters
+    if len(pred_df) == 0:
+        raise Exception("Did not find any enhancers in the Predictions file")
 
-	determine_num_candidate_enh_gene(pred_df, out_file)
+    determine_num_candidate_enh_gene(pred_df, out_file)
+
 
 if __name__ == "__main__":
-	main()
+    main()
--- workflow/scripts/feature_tables/gen_num_sum_nearby_enhancers.py
+++ workflow/scripts/feature_tables/gen_num_sum_nearby_enhancers.py
@@ -2,67 +2,82 @@ import os
 import click
 import pandas as pd
 
+
 def generate_num_sum_enhancers(
-	pred_file, enh_df, distance_threshold, chr_sizes, enh_midpoint, enh_expanded, pred_slim, enh_pred_int, out_num, out_sum
+    pred_file,
+    enh_df,
+    distance_threshold,
+    chr_sizes,
+    enh_midpoint,
+    enh_expanded,
+    pred_slim,
+    enh_pred_int,
+    out_num,
+    out_sum,
 ):
-   # write enhancer midpoint file
-	enh_df["midpoint"] =  ((enh_df["start"] + enh_df["end"]) / 2).astype("int")
-	enh_df[["chr", "midpoint", "midpoint", "name"]].to_csv(
-			enh_midpoint,
-			sep="\t",
-			index=False,
-			header=False,
-		)
+    # write enhancer midpoint file
+    enh_df["midpoint"] = ((enh_df["start"] + enh_df["end"]) / 2).astype("int")
+    enh_df[["chr", "midpoint", "midpoint", "name"]].to_csv(
+        enh_midpoint,
+        sep="\t",
+        index=False,
+        header=False,
+    )
 
-	# expand enhancer to distance bounds
-	os.system(
-		"bedtools slop -b {} -i {} -g {} > {}".format(
-			distance_threshold, enh_midpoint, chr_sizes, enh_expanded
-		)
-	)
-	
-	# select columns from EnhancerPredictionsAllPutative
-	os.system(
-		"zcat {} | csvtk cut -t -f chr,start,end,name,activity_base | sed '1d' > {}".format(
-			pred_file, pred_slim
-		)
-	)
+    # expand enhancer to distance bounds
+    os.system(
+        "bedtools slop -b {} -i {} -g {} > {}".format(
+            distance_threshold, enh_midpoint, chr_sizes, enh_expanded
+        )
+    )
 
-	# intersect with expanded enhancer regions and count n enhancers
-	os.system(
-		"bedtools intersect -a {} -b {} -wa -wb > {} | sort -u ".format(
-			enh_expanded,
-			pred_slim,
-			enh_pred_int,
-		)
-	)
-	
-	# read in interesct file
-	int_df = pd.read_csv(
-		enh_pred_int,
-		sep="\t",
-		header=None,
-	)
+    # select columns from EnhancerPredictionsAllPutative
+    os.system(
+        "zcat {} | csvtk cut -t -f chr,start,end,name,activity_base | sed '1d' > {}".format(
+            pred_file, pred_slim
+        )
+    )
 
-	# 
-	int_df_nonself = int_df[int_df[3] != int_df[7]] # filter out target enhancer from the count 
-	num_enh = int_df_nonself.groupby([3]).size().reset_index(name="count") # count number of intersecting elements per enhancer
-	sum_enh = int_df_nonself.groupby([3])[8].sum().reset_index(name="sum")  # sum activity of intersecting elements per enhancer
-	
-	num_enh.to_csv(
-		out_num,
-		sep="\t",
-		header=False,
-		index=False,
-	)
+    # intersect with expanded enhancer regions and count n enhancers
+    os.system(
+        "bedtools intersect -a {} -b {} -wa -wb > {} | sort -u ".format(
+            enh_expanded,
+            pred_slim,
+            enh_pred_int,
+        )
+    )
 
-	sum_enh.to_csv(
-		out_sum,
-		sep="\t",
-		header=False,
-		index=False,
-	)
+    # read in interesct file
+    int_df = pd.read_csv(
+        enh_pred_int,
+        sep="\t",
+        header=None,
+    )
 
+    #
+    int_df_nonself = int_df[
+        int_df[3] != int_df[7]
+    ]  # filter out target enhancer from the count
+    num_enh = (
+        int_df_nonself.groupby([3]).size().reset_index(name="count")
+    )  # count number of intersecting elements per enhancer
+    sum_enh = (
+        int_df_nonself.groupby([3])[8].sum().reset_index(name="sum")
+    )  # sum activity of intersecting elements per enhancer
+
+    num_enh.to_csv(
+        out_num,
+        sep="\t",
+        header=False,
+        index=False,
+    )
+
+    sum_enh.to_csv(
+        out_sum,
+        sep="\t",
+        header=False,
+        index=False,
+    )
 
 
 @click.command()
@@ -76,15 +91,39 @@ def generate_num_sum_enhancers(
 @click.option("--enh_pred_int")
 @click.option("--out_num")
 @click.option("--out_sum")
+def main(
+    abc_predictions,
+    enhancer_list,
+    distance_threshold_kb,
+    chr_sizes,
+    enh_midpoint,
+    enh_expanded,
+    pred_slim,
+    enh_pred_int,
+    out_num,
+    out_sum,
+):
 
-def main(abc_predictions, enhancer_list, distance_threshold_kb, chr_sizes, enh_midpoint, enh_expanded, pred_slim, enh_pred_int, out_num, out_sum):
+    enh_df = pd.read_csv(
+        enhancer_list, sep="\t", usecols=["chr", "start", "end", "name"]
+    )
+    distance_threshold = (
+        distance_threshold_kb.astype(int) * 1000
+    )  # convert from kb to bp
 
-	enh_df = pd.read_csv(enhancer_list, sep="\t", usecols=["chr", "start", "end", "name"])
-	distance_threshold = distance_threshold_kb.astype(int)*1000 # convert from kb to bp
+    generate_num_sum_enhancers(
+        abc_predictions,
+        enh_df,
+        distance_threshold,
+        chr_sizes,
+        enh_midpoint,
+        enh_expanded,
+        pred_slim,
+        enh_pred_int,
+        out_num,
+        out_sum,
+    )
 
-	generate_num_sum_enhancers(
-		abc_predictions, enh_df, distance_threshold, chr_sizes, enh_midpoint, enh_expanded, pred_slim, enh_pred_int, out_num, out_sum
-	)
 
 if __name__ == "__main__":
-	main()
+    main()
