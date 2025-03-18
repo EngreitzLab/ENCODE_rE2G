fef1badecd2bf86282209c95abd1c3783d829eb0
Maya Sheth <shethm@sh02-ln01.stanford.edu>
2025-03-17 17:44:51 -0700

    update scripts, ABC

--- workflow/scripts/feature_tables/combine_feature_tables_apply.R
+++ workflow/scripts/feature_tables/combine_feature_tables_apply.R
@@ -12,12 +12,12 @@ for (i in 1:length(model_dirs)){
 
 # for sc-E2G pipeline
 if (("ARC.E2G.Score" %in% df$feature) | ("Kendall" %in% df$feature)){
-	ARC_rows = data.frame(c("RNA_meanLogNorm", "RNA_pseudobulkTPM", "RNA_percentCellsDetected", "Kendall", "ARC.E2G.Score", "ABC.Score"),
-		c("mean_log_normalized_rna", "RnaPseudobulkTPM", "RnaDetectedPercent", "Kendall", "ARC.E2G.Score", "ABC.Score"),
-		c(NA, NA, NA, NA, NA, NA),
-		c("mean", "mean", "mean", "max", "mean", "sum"),
-		c(0, 0, 0, 0, 0, 0),
-		c("Mean log normalized RNA expression", "RNA pseudobulk TPM", "RNA percent cells detected", "Kendall correlation", "ARC-E2G score", "ABC score"));
+	ARC_rows = data.frame(c("RNA_meanLogNorm", "RNA_pseudobulkTPM", "RNA_percentCellsDetected", "Kendall", "ARC.E2G.Score", "ABC.Score", "normalizedATAC_enh"),
+		c("mean_log_normalized_rna", "RnaPseudobulkTPM", "RnaDetectedPercent", "Kendall", "ARC.E2G.Score", "ABC.Score", "normalized_atac_enh"),
+		c(NA, NA, NA, NA, NA, NA, NA),
+		c("mean", "mean", "mean", "max", "mean", "sum", "mean"),
+		c(0, 0, 0, 0, 0, 0, 0),
+		c("Mean log normalized RNA expression", "RNA pseudobulk TPM", "RNA percent cells detected", "Kendall correlation", "ARC-E2G score", "ABC score", "ATAC signal at E"));
 
 	colnames(ARC_rows) = colnames(df)
 
